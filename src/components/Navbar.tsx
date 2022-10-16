0b5c1d08212fade2f5dfcf72d97c4681bcd7cc7d
tim <tfierek357@gmail.com>
2022-10-16 19:18:48 -0400

    started navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <nav>
+            <div className="nav-elements">
+                <button>Projects</button>
+                <button>Skills</button>
+                <button>Contact</button>
+            </div>
+
+            <h3 id="name">Tim Fierek</h3>
+
+            <div className="nav-icons">
+                <i className="fa-brands fa-linkedin"></i>
+                <i className="fa-brands fa-square-github"></i>
+                <i className="fa-solid fa-envelope"></i>
+                <i className="fa-solid fa-share-from-square"></i>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
