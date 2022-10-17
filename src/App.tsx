298adc2ea19ce636e21cffd5efa7d9eefdbb844b
tim <tfierek357@gmail.com>
2022-10-17 19:32:40 -0400

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar'; 
+import Project from './components/Project';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Project />
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Navbar() {
     return (
         <nav>
-            <div className="nav-elements">
+            <div className="nav-buttons">
                 <button>Projects</button>
                 <button>Skills</button>
                 <button>Contact</button>
@@ -11,12 +11,13 @@ function Navbar() {
 
             <h3 id="name">Tim Fierek</h3>
 
-            <div className="nav-icons">
+
+            {/* <div className="nav-icons">
                 <i className="fa-brands fa-linkedin"></i>
                 <i className="fa-brands fa-square-github"></i>
                 <i className="fa-solid fa-envelope"></i>
                 <i className="fa-solid fa-share-from-square"></i>
-            </div>
+            </div> */}
         </nav>
     );
 }
