e16d33562f850d2e2f59b388bcfbcb40f2f4793c
tim <tfierek357@gmail.com>
2022-10-30 19:05:27 -0400

    fixing image bug in deployment

--- src/data.tsx
+++ src/data.tsx
@@ -1,8 +1,14 @@
+import steamImg from "./images/steam-games-in-common-img.png";
+import timelyImg from "./images/TimelyEvents-img.png";
+import recipeImg from "./images/recipe-search-hub-img.png";
+import posImg from "./images/java-pos-system-img.png";
+import memeImg from "./images/meme-generator-img.png";
+
 const projects = [
     {
         name: 'Steam Games in Common',
         link: 'https://github.com/timfierek/SteamGamesInCommon',
-        img: '/images/steam-games-in-common-img.png',
+        img: steamImg,
         description: 'Steam Games in Common is a simple to use web application using the Spring Framework and the Valve API to return a list of games that all entered users own. Also supports entering your own steam account and selecting friends to search for games between.',
         skills: [
             'Java',
@@ -14,7 +20,7 @@ const projects = [
     {
         name: 'TimelyEvents',
         link: 'https://github.com/timfierek/TimelyEvents-Calendar-Application',
-        img: '/images/TimelyEvents-img.png',
+        img: timelyImg,
         description: 'TimelyEvents is a company/team focused calendar powered in part by DaysOfTheYear.com. The application supports user registration/login, creating/deleting/updating events, and daily/weekly/monthly view options. It also has a schedule finder feature and an intuitive UI.',
         skills: [
             'Java',
@@ -31,7 +37,7 @@ const projects = [
     {
         name: 'Recipe Search Hub',
         link: 'https://github.com/timfierek/Recipe-Search-Engine-Application',
-        img: '/images/recipe-search-hub-img.png',
+        img: recipeImg,
         description: 'Recipe Search Hub is an extensive recipe searching web application implementing MongoDB and allowing for a user-saved favorites section. Includes comprehensive recipe information for thousands of recipes; directions, ingredients, nutrition, and more.',
         skills: [
             'Java',
@@ -47,7 +53,7 @@ const projects = [
     {
         name: 'Java POS System',
         link: 'https://github.com/timfierek/Point-of-Sale-Application',
-        img: '/images/java-pos-system-img.png',
+        img: posImg,
         description: 'Thourougly tested and thoughtfully designed terminal based point of sale application built in Java. Allows for inventory and price management, and accepts 4 separate payment methods. Also allows for item sales, and auto generated transaction receipts.',
         skills: [
             'Java',
@@ -60,7 +66,7 @@ const projects = [
     {
         name: 'Meme Generator',
         link: 'https://github.com/timfierek/meme-generator',
-        img: '/images/meme-generator-img.png',
+        img: memeImg,
         description: 'Easy to use web application which leverages the power of React and the imgflip API to create memes.',
         skills: [
             'React',
